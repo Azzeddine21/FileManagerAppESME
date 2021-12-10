e370e274ea9890eb866f558b1f3cf0671aa35ccf
Azzeddine Lahmar <azzeddine21.lahmar@gmail.com>
2021-12-10 17:57:45 +0100

    Done

--- app/src/main/java/com/example/androidproject/GoogleDrive.java
+++ app/src/main/java/com/example/androidproject/GoogleDrive.java
@@ -10,16 +10,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -34,8 +42,10 @@ import java.util.Collections;
 public class GoogleDrive extends AppCompatActivity {
     DriveServiceHelper mDriveServiceHelper;
     String path;
-    Button mSignInButton = MainActivity.mSignInButton, mSignOutButton = MainActivity.mSignOutButton;
-    TextView pseudo2 = MainActivity.pseudo;
+    Uri profile;
+    SharedPreferences mSharedPreferences2;
+    SharedPreferences.Editor mEditor2;
+
 
 
 
@@ -43,8 +53,14 @@ public class GoogleDrive extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_google_drive);
         Intent intent = getIntent();
         path = intent.getStringExtra("path");
+        mSharedPreferences2 = getApplicationContext().getSharedPreferences("saveLogin", MODE_PRIVATE);
+        mEditor2 = mSharedPreferences2.edit();
         requestSignin();
 
     }
@@ -86,10 +102,20 @@ public class GoogleDrive extends AppCompatActivity {
                                 .usingOAuth2(GoogleDrive.this, Collections.singleton(DriveScopes.DRIVE_FILE));
                         credential.setSelectedAccount(googleSignInAccount.getAccount());
                         String pseudoString = googleSignInAccount.getDisplayName();
-                        pseudo2.setText(pseudoString);
-                        mSignInButton.setVisibility(View.INVISIBLE);
-                        pseudo2.setVisibility(View.VISIBLE);
-                        mSignOutButton.setVisibility(View.VISIBLE);
+
+                        pseudoString = googleSignInAccount.getDisplayName();
+                        profile = googleSignInAccount.getPhotoUrl();
+                        if(profile == null){
+                            mEditor2.putString("imageView", null);
+                        } else{
+                            mEditor2.putString("imageView", profile.toString() );
+
+                        }
+                        mEditor2.putString("pseudo", pseudoString);
+                        mEditor2.putBoolean("save", true);
+                        mEditor2.commit();;
+
+
                         Drive googleDriveService = new Drive.Builder(
                                 AndroidHttp.newCompatibleTransport(),
                                 new GsonFactory(),
@@ -119,6 +145,7 @@ public class GoogleDrive extends AppCompatActivity {
             @Override
             public void onSuccess(String s) {
                 progressDialog.dismiss();
+                Toast.makeText(getApplicationContext(), "File uploaded on Google Drive", Toast.LENGTH_LONG).show();
                 finish();
 
             }
@@ -127,11 +154,10 @@ public class GoogleDrive extends AppCompatActivity {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         progressDialog.dismiss();
-                        Toast.makeText(getApplicationContext(), "Check yout google Drive Key", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "A problem has been encountered", Toast.LENGTH_LONG).show();
                         finish();
 
                     }
                 });
     }
-
 }
--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -8,12 +8,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
 import com.bumptech.glide.Glide;
-import androidx.fragment.app.Fragment;
 
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -23,35 +24,26 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.android.material.navigation.NavigationView;
+
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     final public String rootFolder = "AndroidProject";
+    private Menu menu;
     public static Button mSignInButton, mSignOutButton;
     public static TextView pseudo;
     GoogleSignInClient mGoogleSignInClient;
-
+    private final String SIMPLE_FRAGMENT_TAG = "myfragtag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,9 +51,6 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSignInButton = (Button) findViewById(R.id.signin);
-        mSignOutButton = (Button) findViewById(R.id.signout);
-        pseudo = (TextView) findViewById(R.id.pseudo);
 
 
         BottomNavigationView bnv = findViewById(R.id.bottom_navigation);
@@ -72,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
                 Bundle bundle;
                 switch(id){
                     case "fichiers":
-                        System.out.println("fichier");
                         bundle = new Bundle();
                         bundle.putString("path", pathRoot());
                         getSupportFragmentManager().beginTransaction()
@@ -82,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
                                 .commit();
                         break;
                     case "favoris":
-                        System.out.println("favoris");
                         bundle = new Bundle();
                         getSupportFragmentManager().beginTransaction()
                                 .setReorderingAllowed(true)
@@ -90,6 +77,13 @@ public class MainActivity extends AppCompatActivity {
                                 .replace(R.id.frame_layout_container, FavoriteFragment.class, bundle)
                                 .commit();
                         break;
+                    case "compte":
+                        bundle = new Bundle();
+                        getSupportFragmentManager().beginTransaction()
+                                .setReorderingAllowed(true)
+                                .addToBackStack(null)
+                                .replace(R.id.frame_layout_container, AccountFragment.class, bundle)
+                                .commit();
                     default:
                         break;
                 }
@@ -109,100 +103,7 @@ public class MainActivity extends AppCompatActivity {
             //permission not allowed
             requestPermission();
         }
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        mSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch (view.getId()){
-                    case R.id.signin:
-                        signIn();
-                        break;
-                }
-            }
-        });
-
-        mSignOutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch (view.getId()) {
-                    case R.id.signout:
-                        signOut();
-                        break;
-
-                }
-
-            }
-        });
-
-
     }
-
-    private void signOut() {
-        mGoogleSignInClient.signOut()
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Toast.makeText(getApplicationContext(), "User SignOut", Toast.LENGTH_SHORT).show();
-                        mSignInButton.setVisibility(View.VISIBLE);
-                        pseudo.setVisibility(View.INVISIBLE);
-                        mSignOutButton.setVisibility(View.INVISIBLE);
-
-
-                    }
-                });
-    }
-
-
-    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if(result.getResultCode() == Activity.RESULT_OK){
-                        Intent data = result.getData();
-                        System.out.println("!!!");
-                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-                        handleSignInResult(task);
-                    }
-                }
-            }
-    );
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            String pseudoString = account.getDisplayName();
-            pseudo.setText(pseudoString);
-            Toast.makeText(this, "Sign-in successful", Toast.LENGTH_SHORT).show();
-                mSignInButton.setVisibility(View.INVISIBLE);
-                pseudo.setVisibility(View.VISIBLE);
-                mSignOutButton.setVisibility(View.VISIBLE);
-
-            //updateMenu();
-
-
-        } catch (ApiException e) {
-            Log.w("Error", "signInResult:failed code=" + e.getStatusCode());
-        }
-    }
-
-    private void updateMenu() {
-        System.out.println("aaa");
-        mSignInButton.setText("Connecté");
-
-    }
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        someActivityResultLauncher.launch(signInIntent);
-    }
-
-
-
-
 
     private boolean checkPermission(){
         int result = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
